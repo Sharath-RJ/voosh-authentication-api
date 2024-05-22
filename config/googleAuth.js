568caae250f5f9bbb1dfc009f199a14095aee1da
Sharath <sharathj116@gmail.com>
2024-05-23 01:28:44 +0530

    google auth completed

--- config/googleAuth.js
+++ config/googleAuth.js
@@ -2,12 +2,10 @@
 const { google } = require("google-auth-library")
 
 const CLIENT_ID = "your_client_id"
-const CLIENT_SECRET = "your_client_secret"
 const REDIRECT_URI = "http://localhost:3000/google/callback" 
 
 const oAuth2Client = new google.auth.OAuth2(
     CLIENT_ID,
-    CLIENT_SECRET,
     REDIRECT_URI
 )
 
