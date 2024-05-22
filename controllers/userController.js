e1a2efe6bbbee4350655b0885c652401c458a7a5
Sharath <sharathj116@gmail.com>
2024-05-23 00:41:02 +0530

    controller for updating profile added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,30 @@
-const userModel= require('../models/userModel');
+const userModel = require("../models/userModel")
 
-
-const getProfile=async(req,res)=>{
-    try{
-        const user=await userModel.findById(req.user.id).select('-password');//avoid showing password in response for security purpose
-        res.status(200).json(user);
-    }catch(error){
-        res.status(500).json({message:error.message});
+const getProfile = async (req, res) => {
+    try {
+        const user = await userModel.findById(req.user.id).select("-password") //avoid showing password in response for security purpose
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
     }
 }
 
-module.exports={getProfile}
+const updateProfile = async (req, res) => {
+        const { name, bio, phone, email, isPublic } = req.body
+        const updatedFields = { name, bio, phone, email, isPublic }
+        if (req.file) updatedFields.profilePicture = req.file.path
+
+        try {
+            const user = await User.findByIdAndUpdate(
+                req.user.id,
+                { $set: updatedFields },
+                { new: true }
+            )
+            res.json(user)
+        } catch (err) {
+            res.status(500).json({message:err.message})
+        }
+   
+}
+
+module.exports = { getProfile, updateProfile }
