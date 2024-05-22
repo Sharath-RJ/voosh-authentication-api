580cfb10d0548ceb84a2bf90bc3a0842d927a07e
Sharath <sharathj116@gmail.com>
2024-05-22 23:59:49 +0530

    user model created

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express= require('express');
 const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 const bodyparser=require('body-parser');
-const userRoutes =require('./routes/userRoutes')
+const userRoutes =require('./routes/user-auth')
 dotenv.config()
 //initializing express application
 const app=express();
---
+++ models/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+   name:{
+    type:String,
+    required:true
+   },
+   email:{
+    type:String,
+    required:true
+   },
+   password:{
+    type:String,
+    required:true
+   },
+   bio:{
+    type:String,
+    required:true
+   },
+   phone:{
+    type:String,
+    required:true 
+   },
+   profilePhoto:{
+    type:String,
+   },
+   isPublic:{
+    type:Boolean,
+    default:true
+   },
+   role:{
+    type:String,
+    default:'user'
+   }
+})
+
+module.exports = mongoose.model('User',UserSchema)
